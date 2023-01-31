642cde3215f2281c8018109f7e06d5a7e42d508a
splicid <luis1abreu11@gmail.com>
2023-01-30 22:36:06 -0500

    Adding section

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Navbar from './navbar/navbar'
 import App from './App'
+import FrontSection from './section/front-section'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
+    <FrontSection />
     <App />
   </React.StrictMode>,
 )
---
+++ src/section/front-section.jsx
@@ -0,0 +1,15 @@
+import "./front-section.css"
+
+
+const frontSection = () => {
+    return(
+        <div className="content">
+            <section className="content-child">
+                Hello World
+            </section>
+        </div>
+    )
+}
+
+
+export default frontSection;
