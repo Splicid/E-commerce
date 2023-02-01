5215f8a23a12b7407b75a45672142bb718057fc2
splicid <luis1abreu11@gmail.com>
2023-01-31 23:58:13 -0500

    Starting Grid file

--- src/grid/grid.jsx
+++ src/grid/grid.jsx
@@ -4,8 +4,13 @@ import "./grid.css"
 
 const Grid = () => {
     return (
-        <div>
-            Test
+        <div className="grid">
+            <div className="card"> One</div>
+            <div className="card"> Two</div>
+            <div className="card"> Three</div>
+            <div className="card"> Four</div>
+            <div className="card"> Five</div>
+            <div className="card"> Six</div>
         </div>
     )
 }
