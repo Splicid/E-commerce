608767d1048334f9fa6ac9c5c7a72e3737010f19
luis <luis1abreu11@gmail.com>
2023-03-15 23:34:57 -0400

    removed dummy data

--- Client/src/grid/grid.jsx
+++ Client/src/grid/grid.jsx
@@ -10,15 +10,13 @@ const Grid = () => {
   
     useEffect(() => {
       async function fetchData() {
-        axios.get('http://localhost:3000/formData')
-        .then(function (response){
-            console.log(response)
-        })
         try {
-            const response = await fetch('https://dummyjson.com/products/');
+            const response = await fetch('http://localhost:3000/formData', {
+                method: "GET"
+            });
             const json = await response.json();
             setDatas(json.products)
-            //console.log(json)
+            console.log(json)
         } catch (err) {
           setError(err);
         }
@@ -30,7 +28,7 @@ const Grid = () => {
     return (
         <section className="grid-content">
             <div className="cards">
-                    {datas.map(element => {
+                    {/* {datas.map(element => {
                         return (
                             <div className="card">
                                 <img src={element.images[0]} className="card-img"/>
@@ -38,7 +36,7 @@ const Grid = () => {
                                 <h1> ${element.price}</h1>
                             </div>
                         )
-                    })}
+                    })} */}
             </div>
         </section>
     )
